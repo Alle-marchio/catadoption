6518f0965d7941e67d4e194514b8866f03052ac2
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2025-05-02 16:53:38 +0200

    fix error about url and template and create home.html and base.html

--- catadoption/urls.py
+++ catadoption/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('purrfect_homes.urls')),  # Include gli URL dell'app purrfect_homes
 ]
 
--- purrfect_homes/urls.py
+++ purrfect_homes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'purrfect_homes'  # Namespace dell'app
+#app_name = 'purrfect_homes'  # Namespace dell'app
 
 urlpatterns = [
     # Home page
