beb1a13b8cf396126395a6dd97f482700e05e9f3
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2025-05-04 20:14:24 +0200

    Revert "New models"

--- catadoption/settings.py
+++ catadoption/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,14 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-#parte aggiunta il 30/04/25
-AUTH_USER_MODEL = 'purrfect_homes.CustomUser'
 
-# Configurazione media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-#fine riga aggiunta
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'purrfect_homes',
+    'purrfect_homes.apps.PurrfectHomesConfig',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +55,7 @@ ROOT_URLCONF = 'catadoption.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR / 'templates')]
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -125,12 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#riga aggiunta il 30/04
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-#fine riga aggiunta
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
